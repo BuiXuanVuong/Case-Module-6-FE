d6ed94a6ab4bc833263db721f80595a2a4e7d8c5
NguyenQuang99 <66399572+NguyenQuang99@users.noreply.github.com>
2021-01-15 14:33:03 +0700

    second1

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -6,6 +6,12 @@ import {IAccount} from '../model/iaccount';
 import {IStatus} from '../model/istatus';
 import {INewfeedResponse} from '../model/inewfeed-response';
 import {IComment} from '../model/icomment';
+import {NofiticationService} from '../service/nofitication.service';
+import {IImage} from '../model/iimage';
+import {HttpClient} from '@angular/common/http';
+import {AngularFireDatabase} from '@angular/fire/database';
+import {Router} from '@angular/router';
+import {$} from 'protractor';
 
 @Component({
   selector: 'app-status',
@@ -14,89 +20,192 @@ import {IComment} from '../model/icomment';
 })
 export class StatusComponent implements OnInit {
 
-  // // @ts-ignore
-  // statuses: IStatus[];
-  // current_id: number;
-  //
-  // newFeedResponse: INewfeedResponse[];
-  //
-  // comments: IComment[];
-  //
-  // delete_comment_id: number;
-  //
-  // status_id: number;
-  //
-  // @Input()
-  // currentAccount: IAccount;
-  //
-  // // @ts-ignore
-  // currentStatus: IStatus = {
-  //   id: 0,
-  //   content: '',
-  //   images: [],
-  //   totalComments: 0,
-  //   totalLikes: 0,
-  //
-  // };
-  //
-  // new_comment: IComment = {
-  //   content: '',
-  //   account: {
-  //     id: this.current_id,
-  //   },
-  //
-  // };
-  //
-  //
-  // status_id_loading_comments: number;
-  //
-  // total_record = 0;
-  // notEmptyRecord = true;
-  // notScroll = true;
-  //
-  //
-  //
-  constructor(private accountService: AccountService,
-              private token: TokenStorageService,
-              private statusService: StatusService,
-              ) {
+  // @ts-ignore
+  listPost: IStatus[];
+  // @ts-ignore
+  post: IStatus;
+  // @ts-ignore
+  user: IAccount;
+  // @ts-ignore
+  images: IImage;
+  // @ts-ignore
+  statusPost: number;
+  // @ts-ignore
+  contentPost: string;
+  arrayPicture = '';
+
+  constructor(private http: HttpClient,
+              private db: AngularFireDatabase,
+              private router: Router) {
   }
 
-  ngOnInit(): void {
-    // this.current_id = this.token.getAccount();
-    // this.getFirstNewFeed();
+  ngOnInit() {
+    this.statusPost = 1;
+    this.getAllPost();
+    this.getImgUserLogin();
   }
-  //
-  //
-  // getFirstNewFeed() {
-  //   this.statusService.getNewFeed2(this.current_id, 0).subscribe(
-  //     (newfeed: any) => {
-  //       this.newFeedResponse = newfeed;
-  //       if (newfeed.length == 0){
-  //         this.notEmptyRecord = false;
-  //       }
-  //       this.total_record += newfeed.length;
-  //       this.newFeedResponse.map(
-  //         status1 =>
-  //           status1.status.createDate = new Date(status1.status.createDate));
-  //     }
-  //   );
-  // }
-  //
-  //
-  // deleteStatus(id: number) {
-  //   this.statusService.deleteStatusById(id).subscribe((response) => {
-  //     if (response.message == 'xóa thành công') {
-  //       this.notice.success('Xóa thành công');
-  //       this.updateNewFeed();
-  //     } else {
-  //       this.notice.fail('Thử lại sau');
-  //     }
-  //   }, () => {
-  //     this.notice.fail('Lỗi kết nối');
-  //   });
-  //
-  // }
+
+  // @ts-ignore
+  setIdUserPost(id) {
+    localStorage.setItem('idUserPost', id);
+    this.router.navigate(['/friendswall']);
+  }
+
+  getAllPost() {
+    // @ts-ignore
+    this.user = JSON.parse(localStorage.getItem('currentUser'));
+    const url = 'http://localhost:8080/api/allPost';
+    this.http.get<IStatus[]>(url).subscribe((resJson) => {
+      this.listPost = resJson;
+      console.log('this.listPost');
+      console.log(this.listPost);
+      this.listPost.reverse();
+      this.getPostShare();
+    });
+  }
+
+  getImgUserLogin() {
+    const url = 'http://localhost:8080/users/' + this.user.id;
+    this.http.get<IAccount>(url).subscribe((resJson) => {
+      this.user = resJson;
+      console.log('this.user');
+      console.log(this.user);
+    });
+  }
+
+  getPostShare() {
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < this.listPost.length; i++) {
+      // @ts-ignore
+      if (this.listPost[i].postIdShear != null) {
+        // @ts-ignore
+        const url = 'http://localhost:8080/api/findPostById/' + this.listPost[i].postIdShear;
+        this.http.get<IStatus>(url).subscribe((resJson) => {
+          // @ts-ignore
+          this.listPost[i].post = resJson;
+          console.log('ppppppppppppppppppppp');
+          // @ts-ignore
+          console.log(this.listPost[i].post);
+        });
+      }
+    }
+  }
+
+  savePost() {
+    // tslint:disable-next-line:max-line-length
+    // @ts-ignore
+    // tslint:disable-next-line:max-line-length
+    this.post = {id: null, createAt: null, notification: null, content: this.contentPost, status: this.statusPost, user: null, postIdShear: null, imgs: this.arrayPicture};
+    const url = 'http://localhost:8080/api/addPost/' + this.user.id;
+    console.log(this.post);
+    this.http.post(url, this.post).subscribe((resJson) => {
+      alert('create thành công');
+    }, error => {
+      alert('create lỗi');
+    });
+  }
+
+  // @ts-ignore
+  updatePostNoImg(id) {
+    // tslint:disable-next-line:max-line-length
+    // @ts-ignore
+    // tslint:disable-next-line:max-line-length
+    this.post = {id, createAt: null, notification: null, content: this.contentPost, status: this.statusPost, user: null, postIdShear: null, imgs: this.arrayPicture};
+    const url = 'http://localhost:8080/api/editPost/' + this.user.id;
+    console.log(this.post);
+    this.http.post(url, this.post).subscribe((resJson) => {
+      alert('edit thành công');
+    }, error => {
+      alert('edit lỗi');
+    });
+  }
+
+  // @ts-ignore
+  deletePost(id) {
+    const url = 'http://localhost:8080/api/deletePost/' + id;
+    this.http.post(url, id).subscribe((resJson) => {
+      alert('remote thành công');
+    }, error => {
+      alert('remote lỗi');
+    });
+  }
+
+  // @ts-ignore
+  sharePost(id) {
+    // tslint:disable-next-line:max-line-length
+    // @ts-ignore
+    // tslint:disable-next-line:max-line-length
+    this.post = {id: null, createAt: null, notification: null, content: this.contentPost, status: this.statusPost, user: null, postIdShear: id, imgs: this.arrayPicture};
+    const url = 'http://localhost:8080/api/addPost/' + this.user.id;
+    console.log(this.post);
+    this.http.post(url, this.post).subscribe((resJson) => {
+      alert('create thành công');
+    }, error => {
+      alert('create lỗi');
+    });
+  }
+
+  // @ts-ignore
+  updatePostAndImg(idPost, idImg, linkImg) {
+    if (this.arrayPicture === '') {
+      this.arrayPicture = linkImg;
+    }
+    // tslint:disable-next-line:max-line-length
+    // @ts-ignore
+    // tslint:disable-next-line:max-line-length
+    this.post = {id: idPost, createAt: null, notification: null, content: this.contentPost, status: this.statusPost, user: null, postIdShear: null, imgs: this.arrayPicture};
+    const url = 'http://localhost:8080/api/editPostAndImg/' + idImg;
+    console.log(idImg);
+    this.http.post(url, this.post).subscribe((resJson) => {
+      alert('edit thành công');
+    }, error => {
+      alert('edit lỗi');
+    });
+  }
+
+  // @ts-ignore
+  saveImg(value) {
+    const file = value.target.files;
+    // @ts-ignore
+    const uploadTask = firebase.storage().ref('img/' + Date.now()).put(file[0]);
+    // @ts-ignore
+
+    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+      // @ts-ignore
+      (snapshot) => {
+        // in progress
+        // @ts-ignore
+        const snap = snapshot as firebase.storage.UploadTaskSnapshot;
+      },
+      () => {
+        console.log('Error');
+      },
+      () => {
+        // @ts-ignore
+        uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
+          this.arrayPicture = downloadURL;
+          console.log(this.arrayPicture);
+        });
+      }
+    );
+  }
+
+  // @ts-ignore
+  showUpdatePost = (id, content, status) => {
+    this.contentPost = content;
+    this.statusPost = status;
+    $('#myModal' + id).modal('show');
+  }
+  // @ts-ignore
+  showSharePost = (id) => {
+    $('#modalShare' + id).modal('show');
+  }
+  closeEditPost = () => {
+    this.contentPost = '';
+    this.statusPost = 1;
+    // @ts-ignore
+    this.arrayPicture = null;
+  }
 
 
 }
