999b9738a85a0f38435001628bf29a223faa9430
unknown <buixuanvuong2410@gmail.com>
2021-01-19 10:03:32 +0700

    2

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,13 +1,39 @@
-import { Injectable } from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
 import {UserToken} from './user-token';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment';
+import {map} from 'rxjs/operators';
 
+const API_URL = `${environment.apiUrl}`;
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   // @ts-ignore
   private currentUserSubject: BehaviorSubject<UserToken>;
+  // @ts-ignore
   private currentUser: Observable<UserToken>;
-  constructor() { }
+  // @ts-ignore
+  update = new EventEmitter<string>();
+  constructor(private http: HttpClient) {
+    // @ts-ignore
+    this.currentUserSubject = new BehaviorSubject<UserToken>(JSON.parse(localStorage.getItem('user')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  // @ts-ignore
+  public get currentUserValue(): UserToken{
+    return this.currentUserSubject.value;
+  }
+
+  login(userName: string, password: string) {
+    return this.http.post(API_URL + '/login', {userName, password})
+      .pipe(map(user => {
+        localStorage.setItem('user', user);
+        this.currentUserSubject.next(user);
+        this.update.emit('user');
+        return user;
+      }));
+  }
 }
