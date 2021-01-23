900fb20965f11c258e88b929a0914b0d83e0d849
unknown <buixuanvuong2410@gmail.com>
2021-01-24 02:07:05 +0700

    24/1

--- src/app/list-friend/list-friend.component.ts
+++ src/app/list-friend/list-friend.component.ts
@@ -1,18 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import {AccountService} from '../service/account.service';
-import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 import {IAccount} from '../model/iaccount';
 import {AuthService} from '../auth.service';
 import {IStatus} from '../model/istatus';
 import {StatusService} from '../service/status.service';
+import {Iuser} from '../model/iuser';
+import {LikeService} from '../service/like.service';
 
 @Component({
   selector: 'app-list-friend',
   templateUrl: './list-friend.component.html',
   styleUrls: ['./list-friend.component.css']
 })
+
+
+
 export class ListFriendComponent implements OnInit {
 
+  // @ts-ignore
+  public userPath: Iuser;
+// @ts-ignore
+  public userLogin: Iuser;
+  public userName: any;
+  public userNamePath: any;
   public id = 0;
   // @ts-ignore
   listFriend: IAccount[];
@@ -22,7 +33,25 @@ export class ListFriendComponent implements OnInit {
               private router: Router,
               private route: ActivatedRoute,
               private auth: AuthService,
-              private statusService: StatusService) { }
+              private statusService: StatusService) {
+
+      this.userNamePath = this.route.snapshot.params.userNamePath;
+
+    //
+    this.route.paramMap.subscribe((paraMap: ParamMap) => {
+      console.log(paraMap.get('userName'));
+      // @ts-ignore
+      accountService.getUserPathByUserName(this.userNamePath).subscribe(data => {
+        this.userPath = data;
+      });
+
+      accountService.getUserPathByUserName(this.auth.currentUserValue.userName).subscribe( data => {
+        this.userLogin = data;
+      });
+
+    });
+
+  }
 
   ngOnInit(): void {
     // @ts-ignore
@@ -51,3 +80,6 @@ export class ListFriendComponent implements OnInit {
     this.router.navigate(['create-message', userFriend]);
   }
 }
+
+
+
